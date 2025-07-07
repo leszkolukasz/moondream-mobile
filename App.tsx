be729de8204b32969bf6b7bb078df242d83e4d54
Łukasz Leszko <leszko.lucas@gmail.com>
2025-07-07 18:16:13 +0200

    Backup

--- App.tsx
+++ App.tsx
@@ -1,58 +1,89 @@
-import { StyleSheet, Button, TextInput, View, Linking, Platform } from 'react-native';
+import { StyleSheet, Button, Image, TextInput, View, Linking, Platform } from "react-native";
 import { InferenceSession, Tensor } from "onnxruntime-react-native";
-import { PermissionsAndroid } from 'react-native';
-import { StorageAccessFramework } from 'expo-file-system';
+import * as FileSystem from "expo-file-system";
+import * as ImagePicker from "expo-image-picker";
+// import * as tf from "@tensorflow/tfjs";
+// import "@tensorflow/tfjs-react-native";
 
 import { Moondream } from "moondream-rn";
 
+import { useEffect, useState } from "react";
 
-import { useEffect, useState } from 'react';
-import { useAssets } from 'expo-asset';
+const downloadModel = async () => {
+    const outDir = FileSystem.documentDirectory + "models/";
+
+    const files = [
+        { url: "https://huggingface.co/whistleroosh/moondream-rn/resolve/main/config.json?download=true", name: "config.json" },
+        { url: "https://huggingface.co/whistleroosh/moondream-rn/resolve/main/coord_decoder.onnx?download=true", name: "coord_decoder.onnx" },
+        { url: "https://huggingface.co/whistleroosh/moondream-rn/resolve/main/coord_encoder.onnx?download=true", name: "coord_encoder.onnx" },
+        { url: "https://huggingface.co/whistleroosh/moondream-rn/resolve/main/initial_kv_cache.json?download=true", name: "initial_kv_cache.json" },
+        { url: "https://huggingface.co/whistleroosh/moondream-rn/resolve/main/size_decoder.onnx?download=true", name: "size_decoder.onnx" },
+        { url: "https://huggingface.co/whistleroosh/moondream-rn/resolve/main/size_encoder.onnx?download=true", name: "size_encoder.onnx" },
+        { url: "https://huggingface.co/whistleroosh/moondream-rn/resolve/main/text_decoder.onnx?download=true", name: "text_decoder.onnx" },
+        { url: "https://huggingface.co/whistleroosh/moondream-rn/resolve/main/text_encoder.onnx?download=true", name: "text_encoder.onnx" },
+        { url: "https://huggingface.co/whistleroosh/moondream-rn/resolve/main/tokenizer.json?download=true", name: "tokenizer.json" },
+        { url: "https://huggingface.co/whistleroosh/moondream-rn/resolve/main/vision_encoder.onnx?download=true", name: "vision_encoder.onnx" },
+        { url: "https://huggingface.co/whistleroosh/moondream-rn/resolve/main/vision_projection.onnx?download=true", name: "vision_projection.onnx" }
+    ];
+
+    console.log("Downloading model files...");
+
+    await FileSystem.makeDirectoryAsync(outDir, { intermediates: true });
+
+    for (const file of files) {
+        const fileOutputUri = outDir + file.name;
+        const fileExists = await FileSystem.getInfoAsync(fileOutputUri);
+        if (fileExists.exists) {
+            console.log(`File ${file.name} already exists, skipping download.`);
+            continue;
+        }
+
+        const result = await FileSystem.downloadAsync(file.url, fileOutputUri);
+
+        if (result.status !== 200) {
+            throw new Error(`Failed to download ${file.name}`);
+        }
+
+        console.log(`Downloaded and saved ${file.name} to ${fileOutputUri}`);
+    }
+}
 
 export default function ModelScreen() {
     const [moondream, setMoondream] = useState<Moondream | null>(null);
-    const [xValue, setXValue] = useState<string>(''); // State for x input
-    const [yValue, setYValue] = useState<string>(''); // State for y input
+    const [imageURI, setImageURI] = useState<string | null>(null);
 
     useEffect(() => {
         async function loadModel() {
-            const granted = await PermissionsAndroid.request(
-                PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
-                {
-                    title: 'Cool Photo App Camera Permission',
-                    message:
-                    'Cool Photo App needs access to your camera ' +
-                    'so you can take awesome pictures.',
-                    buttonNeutral: 'Ask Me Later',
-                    buttonNegative: 'Cancel',
-                    buttonPositive: 'OK',
-                },
-            );
-
-            console.log("Permission granted:", granted);
-            console.log(await PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE));
-
-            // const permissions = await StorageAccessFramework.requestDirectoryPermissionsAsync();
-            // console.log("Permissions granted:", permissions);
-
             try {
-                const moondream: Moondream = await Moondream.load("/sdcard/moondream/models")
+                await downloadModel();
+                const moondream: Moondream = await Moondream.load(FileSystem.documentDirectory + "models/");
                 setMoondream(moondream);
                 console.log("Model loaded successfully");
             } catch (error) {
-                console.error("Error loading model", error);
+                console.error("Error: ", error);
             }
         }
         loadModel();
     }, []);
 
-    console.log(Platform.Version, moondream);
+    const pickImage = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ["images", "videos"],
+            // allowsEditing: true,
+            // aspect: [4, 3],
+            quality: 1,
+        });
+
+        if (!result.canceled) {
+            setImageURI(result.assets[0].uri);
+        }
+    };
+
+    // console.log(tf.tensor1d([1, 2, 3]).toString());
 
     const execute = async () => {
         // const tensorA = new Tensor('float32', Float32Array.from([parseFloat(xValue)]), [1]);
         // const tensorB = new Tensor('float32', Float32Array.from([parseFloat(yValue)]), [1]);
-
-        console.log("Executing model with inputs:", xValue, yValue);
 
         const input_embeds = new Tensor('float32', Float32Array.from(Array(5*1024).fill(0.5)), [1, 5, 1024]);
         console.log("Created input_embeds tensor");
@@ -84,21 +115,9 @@ export default function ModelScreen() {
     return (
         <View>
             <View style={styles.inputContainer}>
-                <TextInput
-                    style={styles.input}
-                    placeholder="Enter x value"
-                    keyboardType="numeric"
-                    value={xValue}
-                    onChangeText={setXValue}
-                />
-                <TextInput
-                    style={styles.input}
-                    placeholder="Enter y value"
-                    keyboardType="numeric"
-                    value={yValue}
-                    onChangeText={setYValue}
-                />
+                <Button title="Pick an image from camera roll" onPress={pickImage} />
             </View>
+            {imageURI && <Image source={{ uri: imageURI }} style={styles.image}/>}
             <Button title="Run Model" onPress={execute} />
         </View>
     );
@@ -115,5 +134,9 @@ const styles = StyleSheet.create({
         marginBottom: 10,
         paddingHorizontal: 8,
         borderRadius: 5,
+    },
+    image: {
+        width: 400,
+        height: 400,
     },
 });
